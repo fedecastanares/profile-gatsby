09db3cdbb59b87a991cff4e1677c1ab1faae3cba
Federico Castañares <federico.castanares@gmail.com>
2020-05-27 22:51:19 -0300

    fix sizes

---
+++ src/components/whoiam.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Typography, Grid, Container} from '@material-ui/core'
+import ProfileImg from './profileimg.js'
+
+
+const useStyles = makeStyles(theme => ({
+    root: {height: '100%', paddingTop: '20vh'},
+    typography: {
+        color: 'white',
+        fontSize: '1rem',
+        marginLeft: '5vh',
+        [theme.breakpoints.up('sm')]: {
+            padding: '1vh 0 1vh 0',
+            fontSize: '1.6rem',
+            marginLeft: 0,
+          },
+    },
+    name: {
+        marginLeft: '5vh',
+        fontSize: '2rem',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: 0,
+            fontSize: '2.6rem',
+          },
+    }
+    
+
+}));
+
+const WhoAmi = () => {
+    const classes = useStyles();
+
+    return ( 
+        <>
+        <Container className={classes.root}>
+          <Grid container justify={window.innerWidth > 768 ? 'space-evenly' : 'center'} alignContent='center' alignItems='center' style={{height: '100%'}}>
+            <Grid item style={{paddingBottom: '15vh'}}>
+              <Typography className={classes.name} color='primary' variant="h3" gutterBottom>
+              Federico Castañares
+              </Typography>
+              <Typography className={classes.typography} variant="h4" gutterBottom>
+              Desarrollador FullStack
+              </Typography>
+              <Typography className={classes.typography} variant="h5" gutterBottom>
+              Edad: 25 años
+              </Typography>
+              <Typography className={classes.typography} variant="h5" gutterBottom>
+              Email: federico.castanares@gmail.com
+              </Typography>
+            </Grid>
+            <Grid item>
+              {window.innerWidth > 768 ? <ProfileImg size={{height: '45vh', width: '30vh'}}/> : null}
+            </Grid>
+          </Grid>
+        </Container>
+        </>
+     );
+}
+ 
+export default WhoAmi;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,36 +1,16 @@
 import React from "react"
 import Layout from '../components/layout.js'
 import BackgroundImg from '../components/backgroundimg.js'
-import ProfileImg from '../components/profileimg.js'
 import Greeting from '../components/greeting.js'
-import {Typography, Grid, Container, Paper} from '@material-ui/core'
+import { Paper} from '@material-ui/core'
+import WhoAmi from '../components/whoiam.js'
 
 
-const IndexPage = () =>  (
+const IndexPage = () => (
   <>
     <Layout>
       <BackgroundImg>
-        <Container style={{height: '100%', paddingTop: '20vh'}}>
-          <Grid container justify={window.innerWidth > 768 ? 'space-evenly' : 'center'} alignContent='center' alignItems='center' style={{height: '100%'}}>
-            <Grid item style={{paddingBottom: '15vh'}}>
-              <Typography color='primary' variant="h3" gutterBottom>
-              Federico Castañares
-              </Typography>
-              <Typography style={{color: 'white'}} variant="h4" gutterBottom>
-              Desarrollador FullStack
-              </Typography>
-              <Typography style={{color: 'white'}} variant="h5" gutterBottom>
-              Edad: 25 años
-              </Typography>
-              <Typography style={{color: 'white'}} variant="h5" gutterBottom>
-              Email: federico.castanares@gmail.com
-              </Typography>
-            </Grid>
-            <Grid item>
-              {window.innerWidth > 768 ? <ProfileImg size={{height: '45vh', width: '30vh'}}/> : null}
-            </Grid>
-          </Grid>
-        </Container>
+        <WhoAmi/>
       </BackgroundImg>
       <Paper>
         <Greeting/>
