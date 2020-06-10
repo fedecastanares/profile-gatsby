c6b8aeb41f27a5fb3f5816b62a8d29eaa474e50f
Federico Castañares <federico.castanares@gmail.com>
2020-06-09 22:16:16 -0300

    padding left en projects

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -29,7 +29,7 @@ const Greeting = () => {
                 <Typography variant="h4" gutterBottom>Hola! <span role="img" aria-label="Waving Hand">👋</span></Typography >
                 <Typography className={classes.about} variant="body1" gutterBottom>Soy desarrollador Web, desde tecnologias como jQuery<span role="img" aria-label="Old Man">👴</span> hasta modernas como Gatsby (Este sitio esta hecho con Gatsby <span role="img" aria-label="Smiling Face with Sunglasses">😎</span>). </Typography>
                 <Typography className={classes.about} variant="body1" gutterBottom>Comence con el bachillerato tecnologico y luego fui hacia la ruta web, especializandome en interfaces con React <span role="img" aria-label="Atom Symbol">⚛️</span>. </Typography>
-                <Typography className={classes.about} variant="body1" gutterBottom><br/><span style={{fontStyle: 'italic', fontWeight: 400}}>Disfruta del camino.</span> <br/>Tambien disfruto al aire libre y  juego a videojuegos, soy jugador de clubes pro y COD.</Typography>
+                <Typography className={classes.about} variant="body1" gutterBottom><br/><span style={{fontStyle: 'italic', fontWeight: 400}}>Disfruta del camino.</span></Typography>
             </Container>
         </div>
         </>
